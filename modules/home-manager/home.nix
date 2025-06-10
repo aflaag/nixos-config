06bca6fb71c732c9f18c4a8670cce512958f4a5f
aflaag <alessio.bandiera02@gmail.com>
2025-06-10 14:04:29 +0200

    snapshot for working version

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -80,4 +80,11 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # programs.gh = {
+  #   enable = true;
+  #   gitCredentialHelper = {
+  #     enable = true;
+  #   };
+  # };
 }
